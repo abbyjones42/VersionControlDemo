dde6eba53830816cd940c4dca32e763610e57922
Abby Jones <ajones42@dmacc.edu>
2024-09-29 14:39:05 -0500

    Initial Commit.

---
+++ src/main/java/com/mycompany/versioncontrol_abigailjones/VersionControl_AbigailJones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_abigailjones;
+
+/**
+ *
+ * @author abiga
+ */
+public class VersionControl_AbigailJones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
